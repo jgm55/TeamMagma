8f8cff4eb0328e3877afa33a1614f5d72bc1d5bc
Joe Muoio <jgm55@drexel.edu>
2015-04-13 21:54:55 -0400

    Adding in Title Screen

---
+++ Assets/scripts/TitleScreenButtonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+
+public class TitleScreenButtonController : MonoBehaviour {
+
+    public Sprite good;
+    public Sprite bad;
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+	    if(Properties.lastWin == Properties.PlayStyle.BAD){
+            GetComponent<SpriteRenderer>().sprite = bad;
+        }
+        else if (Properties.lastWin == Properties.PlayStyle.GOOD)
+        {
+            GetComponent<SpriteRenderer>().sprite = good;
+        }
+	}
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
