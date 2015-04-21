a29269ef5c3f1fc7d5444c86fa679990f6152f0d
Joe Muoio <jgm55@drexel.edu>
2015-04-21 14:41:20 -0400

    Adding totems with animation on one. Animation does not work properly however

---
+++ Assets/scripts/TotemController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+
+public class TotemController : MonoBehaviour {
+
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Properties.lastPlayedStyle == Properties.PlayStyle.BAD){
+            Debug.Log("updating bad animation");
+            animator.SetBool("isAngry", true);
+        } else if(Properties.lastPlayedStyle == Properties.PlayStyle.GOOD){
+            Debug.Log("updating good animation");
+            animator.SetBool("isHappy", true);
+        } else {
+            Debug.Log("updating nuetral animation");
+            animator.SetBool("isNuetral", true);
+        }
+	}
+}
