45d8de539e22bc614d9c35cf7966ac0f06e244c5
Josh-Kohn <jkohnbw@gmail.com>
2015-05-10 16:20:00 -0400

    GuiController.cs Added

---
+++ Assets/scripts/GUIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+
+
+public class GUIController : MonoBehaviour {
+
+	public Texture2D goodGUI;
+	public Texture2D badGUI;
+
+	Properties.PlayStyle last = Properties.PlayStyle.NUETRAL;
+
+
+	void OnGUI () {
+		if(Properties.lastPlayedStyle == Properties.PlayStyle.BAD){
+			GUI.DrawTexture(Rect(1,1,0,.934), badGUI, ScaleMode.ScaleToFit, true, 10.0f);
+			Debug.Log("updating bad GUI");
+			("isAngry", true);
+		} else if(Properties.lastPlayedStyle == Properties.PlayStyle.GOOD){
+			GUI.DrawTexture(Rect(1,1,0,.934), goodGUI, ScaleMode.ScaleToFit, true, 10.0f);
+			Debug.Log("updating good GUI");
+			("isHappy", true);
+		} else {
+			Debug.Log("updating nuetral GUI");
+			("isNuetral", true);
+		}
+	}
+}
