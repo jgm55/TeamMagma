443626f895ccdf4815e4e8abc15221bc7f95ebc3
Joe Muoio <jgm55@drexel.edu>
2015-05-13 14:42:54 -0400

    Adding level one island

---
+++ Assets/scripts/LevelController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+    public GameObject[] levels;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
