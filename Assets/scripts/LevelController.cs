dbd1f96c695bc7b4e06701763014bfc2200813a8
Joe Muoio <jgm55@drexel.edu>
2015-05-13 21:23:15 -0400

    Adding Basic New Level Spawning

--- Assets/scripts/LevelController.cs
+++ Assets/scripts/LevelController.cs
@@ -4,14 +4,37 @@ using System.Collections;
 public class LevelController : MonoBehaviour {
 
     public GameObject[] levels;
+    int levelNumber = 0;
+    public GameObject currentLevel;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+
+    /**
+     * Returns true if the game is out of levels. False otherwise
+     * */
+    public bool erupt(){
+        levelNumber++;
+        Debug.Log("ERUPT: " + levelNumber);
+        if (levelNumber == levels.Length)
+        {
+            //GAME IS OVER
+            return true;
+        }
+        GameObject nextLevel = Instantiate(levels[levelNumber]) as GameObject;
+        Debug.Log("child " + currentLevel.transform.childCount);
+        for (int i = 0; i < currentLevel.transform.childCount;i++)
+        {
+            Transform trans = currentLevel.transform.GetChild(i);
+            trans.SetParent(nextLevel.transform);
+        }
+        Destroy(currentLevel);
+        currentLevel = nextLevel;
+        Debug.Log("DoneErupt");
+        return false;
+    }
+
 }
