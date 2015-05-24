139a7fe1e7e92a5d0822871a0b0b9a08b0426068
Josh-Kohn <jkohnbw@gmail.com>
2015-05-24 14:20:59 -0400

    Added in Screen shake code (Isn't broken but doesn't work correctly)

---
+++ Assets/scripts/Screenshake.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+
+public class Screenshake : MonoBehaviour {
+
+	public Camera mainCamera;
+	float shake =2f;
+	float shakeAmt =0.7f;
+	float decreaseFactor = 1.0f;
+
+		void Update() {
+	
+			if (shake > 0) {
+			mainCamera.transform.localPosition = Random.insideUnitSphere * shakeAmt;
+			shake -= Time.deltaTime * decreaseFactor;
+			Debug.Log("shaking");
+		
+			} else {
+				shake = 0;
+					}
+		
+			}
+
+	}
