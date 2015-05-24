2efa651adc77b298eed4293810123dffa08573e7
Josh-Kohn <jkohnbw@gmail.com>
2015-05-24 14:26:42 -0400

    Changed GUIController.cs, added in grunge effect for bad play through

--- Assets/scripts/GUIController.cs
+++ Assets/scripts/GUIController.cs
@@ -9,20 +9,24 @@ public class GUIController : MonoBehaviour {
 	public Image goodGUI;
 	public Image badGUI;
 	public Bloom goodBloom;
+	public Image grunge;
 	
 	void OnGUI () {
 		if(Properties.lastPlayedStyle == Properties.PlayStyle.BAD){
 			badGUI.enabled = true;
 			goodGUI.enabled = false;
 			goodBloom.enabled = false;
+			grunge.enabled = true;
 		} else if(Properties.lastPlayedStyle == Properties.PlayStyle.GOOD){
 			badGUI.enabled = false;
 			goodGUI.enabled = true;
 			goodBloom.enabled = true;
+			grunge.enabled = false;
 		} else {
 			badGUI.enabled = false;
 			goodGUI.enabled = false;
 			goodBloom.enabled = false;
+			grunge.enabled = false;
 		}
 	}
 }
