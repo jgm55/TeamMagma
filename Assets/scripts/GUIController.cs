566655cacd691d6049e1b7e9fc863fc6157b25fc
Josh-Kohn <jkohnbw@gmail.com>
2015-05-25 23:21:10 -0400

    Changed GUI controller slightly for testing.  Writes to log.

--- Assets/scripts/GUIController.cs
+++ Assets/scripts/GUIController.cs
@@ -4,25 +4,29 @@ using Assets.scripts;
 using UnityEngine.UI;
 using UnityStandardAssets.ImageEffects;
 
+
 public class GUIController : MonoBehaviour {
 	
 	public Image goodGUI;
 	public Image badGUI;
 	public Bloom goodBloom;
-
+	
 	void OnGUI () {
 		if(Properties.lastPlayedStyle == Properties.PlayStyle.BAD){
 			badGUI.enabled = true;
 			goodGUI.enabled = false;
 			goodBloom.enabled = false;
+			Debug.Log("updating bad GUI");
 		} else if(Properties.lastPlayedStyle == Properties.PlayStyle.GOOD){
 			badGUI.enabled = false;
 			goodGUI.enabled = true;
 			goodBloom.enabled = true;
+			Debug.Log("updating good GUI");
 		} else {
 			badGUI.enabled = false;
 			goodGUI.enabled = false;
 			goodBloom.enabled = false;
+			Debug.Log("updating nuetral GUI");
 		}
 	}
 }
