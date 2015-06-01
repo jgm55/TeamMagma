f04f6c75f4b07188bf92ca53f07485ffc8df1bf1
Joe Muoio <jgm55@drexel.edu>
2015-06-01 13:58:35 -0400

    Adding flowers

---
+++ Assets/scripts/PlantScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.scripts;
+
+public class PlantScript : MonoBehaviour {
+
+    public Sprite nuetral;
+    public Sprite good;
+    public Sprite bad;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+    //TODO add more interaction?
+	
+	// Update is called once per frame
+	void Update () {
+        if (Properties.lastPlayedStyle == Properties.PlayStyle.BAD)
+        {
+            GetComponent<SpriteRenderer>().sprite = bad;
+        }
+        else if (Properties.lastPlayedStyle == Properties.PlayStyle.GOOD)
+        {
+            GetComponent<SpriteRenderer>().sprite = good;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = nuetral;
+        }
+	}
+}
